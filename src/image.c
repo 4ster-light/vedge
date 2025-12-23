ee930c3d289fd81a341ff7334a30d7e758bfdab2
4ster-light <davidvivarbogonez@gmail.com>
2025-12-23 11:08:25 +0100

    fix: Correct PPM header validation and create grayscale image

--- src/image.c
+++ src/image.c
@@ -16,24 +16,40 @@ Image load_ppm(const char *filename)
 
 	char magic[3];
 	int width, height, maxval;
-	fscanf(fp, "%s %d %d %d", magic, &width, &height, &maxval);
-	if (magic[1] != '6')
+
+	// Check header
+	if (fscanf(fp, "%2s %d %d %d", magic, &width, &height, &maxval) != 4)
+	{
+		fprintf(stderr, "Invalid PPM header\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Check PPM format P6
+	if (magic[0] != 'P' || magic[1] != '6')
 	{
 		fprintf(stderr, "Only P6 PPM format supported.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	Image img = {width, height, malloc(width * height * sizeof(unsigned char))};
-	if (!img.data)
+	// Create grayscale Image
+	Image grayscale = {width, height, malloc(width * height)};
+	if (!grayscale.data)
 	{
 		perror("Image allocation failed");
 		exit(EXIT_FAILURE);
 	}
+	fgetc(fp); // Skip exactly one whitespace character after maxval
 
-	fgetc(fp); // Skip newline
-	fread(img.data, sizeof(unsigned char), width * height, fp);
+	// Convert from RGB
+	unsigned char rgb[3];
+	for (int i = 0; i < width * height; i++)
+	{
+		fread(rgb, sizeof(unsigned char), 3, fp);
+		grayscale.data[i] = (unsigned char)(0.299f * rgb[0] + 0.587f * rgb[1] + 0.114f * rgb[2]);
+	}
+
 	fclose(fp);
-	return img;
+	return grayscale;
 }
 
 void save_ppm(const char *filename, Image img)
@@ -45,8 +61,16 @@ void save_ppm(const char *filename, Image img)
 		exit(EXIT_FAILURE);
 	}
 
+	// Write as P6 (color) so standard image viewers can read it
+	// Write the grayscale value to RGB channels
 	fprintf(fp, "P6\n%d %d\n255\n", img.width, img.height);
-	fwrite(img.data, sizeof(unsigned char), img.width * img.height, fp);
+	for (int i = 0; i < img.width * img.height; i++)
+	{
+		unsigned char gray = img.data[i];
+		fputc(gray, fp);
+		fputc(gray, fp);
+		fputc(gray, fp);
+	}
 	fclose(fp);
 }
 
